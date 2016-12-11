e1dda67bd01315cdd32b3a39566f9029e2aae791
Robo Rick <robo.rick@techwarriorz.com>
2016-12-11 13:54:20 -0800

    whoops

--- server/database/database.js
+++ server/database/database.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-var yourDB = "postgres://blonstyjlevsxf:xh9kadztg8U0BfQTt3g-SeZ3vo@ec2-107-22-223-6.compute-1.amazonaws.com:5432/d3c5dnu383ku2t";
+var yourDB = "INSERT YOUR POSTGRES LINK HERE!";
 var sequelize = new Sequelize(yourDB, {
     dialect: 'postgres',
     dialectOptions: {
